08d4f78ae99cc5c55f2ba67650f64aa39a717f79
Ninjasquirrel220 <tyler.samay@hotmail.com>
2024-11-06 15:51:33 -0500

    debugging
    testing connection pools

--- JS/database.js
+++ JS/database.js
@@ -22,70 +22,114 @@ const config = {
   requestTimeout: 30000,
 };
 
+// Create a connection pool
+let poolPromise = sql.connect(config)
+  .then(pool => {
+    console.log("Connected to the database through connection pool!");
+    return pool;
+  })
+  .catch(err => {
+    console.error("Database Connection Failed! Error: ", err);
+    throw err;
+  });
 
-
-console.log("hi");
-// Get files from folder 
-app.use(express.static(path.join(__dirname, 'public')));
-
-//console.log("hi2");
-
-// Function to get messages from the database
+// Function to get messages from the database using the pool
 async function getMessages() {
-  try{
-    console.log("attempting connection");
-    const pool = await sql.connect(config);
-    console.log("connected");
-
+  try {
+    console.log("Attempting to query messages...");
+    const pool = await poolPromise;
     const result = await pool.request().query('SELECT * FROM dbo.Messagess');
-    pool.close();
-    
-    console.log("messages retrieved:", result.recordset);
+    console.log("Messages retrieved:", result.recordset);
     return result.recordset;
-  } 
-  catch (err){
+  } catch (err) {
     console.error('Database query error:', err);
     throw new Error('Error querying the database.');
   }
-  // finally{
-  //   await sql.close();
-  // }
-};
+}
 
-// When script starts
-// (async () => {
-//   try {
-//     const messages = await getMessages();
-//     console.log('Messages on startup:', messages);
-//   } catch (err) {
-//     console.error('Error during startup message fetch:', err);
-//   }
-// })();
-
-  // API endpoint to get messages
-  
-  app.use((req, res, next) => {
-    console.log(`Incoming request: ${req.method} ${req.url}`);
-    next();
-  });
-  
-
+// API endpoint to get messages
 app.get('/api/messages', async (req, res) => {
-  try{
+  try {
     const messages = await getMessages();
     res.json(messages);
-  } 
-  catch (err){
-    console.error('Error:', err);
+  } catch (err) {
+    console.error('Error in API endpoint:', err);
     res.status(500).send('Error querying the database.');
   }
 });
 
-  app.listen(PORT, '0.0.0.0', () => {
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Start the server
+app.listen(PORT, () => {
   console.log(`Server running at http://prismaverse.csh.rit.edu:${PORT}`);
 });
+
+module.exports = { getMessages };
+
+// console.log("hi");
+// // Get files from folder 
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// //console.log("hi2");
+
+// // Function to get messages from the database
+// async function getMessages() {
+//   try{
+//     console.log("attempting connection");
+//     const pool = await sql.connect(config);
+//     console.log("connected");
+
+//     const result = await pool.request().query('SELECT * FROM dbo.Messagess');
+//     pool.close();
+    
+//     console.log("messages retrieved:", result.recordset);
+//     return result.recordset;
+//   } 
+//   catch (err){
+//     console.error('Database query error:', err);
+//     throw new Error('Error querying the database.');
+//   }
+//   // finally{
+//   //   await sql.close();
+//   // }
+// };
+
+// // When script starts
+// // (async () => {
+// //   try {
+// //     const messages = await getMessages();
+// //     console.log('Messages on startup:', messages);
+// //   } catch (err) {
+// //     console.error('Error during startup message fetch:', err);
+// //   }
+// // })();
+
+//   // API endpoint to get messages
+  
+//   app.use((req, res, next) => {
+//     console.log(`Incoming request: ${req.method} ${req.url}`);
+//     next();
+//   });
+  
+
+// app.get('/api/messages', async (req, res) => {
+//   try{
+//     const messages = await getMessages();
+//     res.json(messages);
+//   } 
+//   catch (err){
+//     console.error('Error:', err);
+//     res.status(500).send('Error querying the database.');
+//   }
+// });
+
+//   app.listen(PORT, '0.0.0.0', () => {
+//   console.log(`Server running at http://prismaverse.csh.rit.edu:${PORT}`);
+// });
 
 //getMessages();
 
 //export {getMessages};
-module.exports = {getMessages};
+//module.exports = {getMessages};
