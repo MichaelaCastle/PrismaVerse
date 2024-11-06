547aad5c8a87dc85c680e2b9ce89e710c589039c
Ninjasquirrel220 <tyler.samay@hotmail.com>
2024-11-05 21:50:08 -0500

    debugging

--- JS/database.js
+++ JS/database.js
@@ -60,6 +60,12 @@ async function getMessages() {
 
   // API endpoint to get messages
   
+  app.use((req, res, next) => {
+    console.log(`Incoming request: ${req.method} ${req.url}`);
+    next();
+  });
+  
+
 app.get('/api/messages', async (req, res) => {
   try{
     const messages = await getMessages();
